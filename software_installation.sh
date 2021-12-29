6971840fe68af414b778e803baaf54b65bc76289
Mikhail Sandakov <msandakov@plesk.com>
2021-12-29 17:03:46 +0700

    Add some zsh plugins

--- software_installation.sh
+++ software_installation.sh
@@ -5,13 +5,18 @@ if command -v "apt" &> /dev/null; then
     package_util="apt-get"
 fi
 
-## ZSH and oh-my-zsh installation
 "$package_util" install -y zsh git screen
+
+# oh-my-zsh installation
 chsh -s $(which zsh)
 wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sed -i 's/  read -r opt/  opt\=y/' install.sh # Avoid asking anything
 chmod 755 install.sh
 RUNZSH=no ./install.sh
 rm install.sh
+
+# And some little plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 
 sed -i '/^ZSH_THEME/s/robbyrussell/afowler/' ~/.zshrc
