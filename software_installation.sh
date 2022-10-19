6fab801a60911ddfebb9dfc7e419594cba790389
Mikhail Sandakov <msandakov@plesk.com>
2022-10-19 10:55:40 +0300

    Move to fish instead of zsh

--- software_installation.sh
+++ software_installation.sh
@@ -7,7 +7,7 @@ fi
 
 DISTRO_NAME=$(grep PRETTY_NAME /etc/os-release | cut -d '"' -f 2)
 
-PACKAGES_TO_INSTALL=(zsh git jq gdb python3-pip)
+PACKAGES_TO_INSTALL=(fish git jq gdb python3-pip)
 IS_NAVI_AVAILABLE=false
 
 case "$DISTRO_NAME" in
@@ -27,35 +27,20 @@ fi
 
 "$package_util" install -y "${PACKAGES_TO_INSTALL[@]}"
 
-# oh-my-zsh installation
-chsh -s $(which zsh)
-wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sed -i 's/  read -r opt/  opt\=y/' install.sh # Avoid asking anything
-chmod 755 install.sh
-RUNZSH=no ./install.sh
-rm install.sh
+# Configure fish and plugins
+chsh -s $(which fish)
 
-# And some little plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
-
-sed -i '1 i\export PATH=$HOME/.cargo/bin:$HOME/bin:/usr/local/bin:$PATH' ~/.zshrc
-sed -i '/^ZSH_THEME/s/robbyrussell/afowler/' ~/.zshrc
-
-# Some utilityes from github
-git clone https://github.com/rupa/z.git
-sed -i -e '$a. ~/z/z.sh' ~/.zshrc
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/z # Analog of z utility but as plugin in fish
 
 if "$IS_NAVI_AVAILABLE" ; then
+    PATH="$HOME/.cargo/bin:$PATH"
+    echo 'set PATH $HOME/.cargo/bin $PATH' >> /etc/fish/config.fish
     git clone --depth 1 https://github.com/junegunn/fzf.git
     ~/fzf/install --completion --key-bindings --no-update-rc
-    sed -i -e '$a [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh' ~/.zshrc
-    source .zshrc
 
     # require 2021 version of rust, so could not be installed on some distros (Rocky linux 8 for example)
     cargo install --locked navi
-    navi repo add https://github.com/SandakovMM/my_navi_cheats
-    navi repo add https://git.plesk.ru/scm/~msandakov/plesk-cheats-for-navi.git
+    fish -c "navi repo add https://github.com/SandakovMM/my_navi_cheats"
+    fish -c "navi repo add https://git.plesk.ru/scm/~msandakov/plesk-cheats-for-navi.git"
 fi
-
-source .zshrc
